c9cb6271b0c8cfece3a6e1d13315107c63526af7
Cédric Krier <ced@b2ck.com>
2009-08-24 17:17:49 +0200

    Prevent reloading caldav

--- caldav.py
+++ caldav.py
@@ -1,5 +1,6 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
+_TRYTON_RELOAD = False
 
 from DAV import propfind
 from DAV.errors import *
