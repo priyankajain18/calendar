c343eafef5fd45a01d55f2ea6f9b58a8af5cfe36
Cédric Krier <ced@b2ck.com>
2012-10-10 01:05:11 +0200

    Fix domain concatenation between list and tuple

--- calendar_.py
+++ calendar_.py
@@ -542,7 +542,7 @@ class Event(ModelSQL, ModelView):
 
     @classmethod
     def search_calendar_field(cls, name, clause):
-        return [('calendar.' + name[9:],) + clause[1:]]
+        return [('calendar.' + name[9:],) + tuple(clause[1:])]
 
     def check_recurrence(self):
         '''
