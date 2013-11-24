06b67a516413f3167345d1a25ceda90c395a6256
Cédric Krier <ced@b2ck.com>
2013-11-24 20:24:21 +0100

    Allow empty week day on recurring rule (transplanted from 3254d1697367ab819e73ae721deebdbb5e9b1e56)
    --HG--
    extra : transplant_source : 2T%D1isg%AB%81%9Es%AEr%1D%EE%BD%BB%5E%9B%1EV

--- calendar_.py
+++ calendar_.py
@@ -1765,6 +1765,7 @@ class RRule(ModelSQL, ModelView):
     bymonth = fields.Char('By Month')
     bysetpos = fields.Char('By Position')
     wkst = fields.Selection([
+        (None, ''),
         ('su', 'Sunday'),
         ('mo', 'Monday'),
         ('tu', 'Tuesday'),
