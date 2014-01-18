651156b3a127577267d81ed36b70921d80d030c9
Cédric Krier <ced@b2ck.com>
2014-01-18 20:52:54 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -11,7 +11,7 @@
     'name_es_ES': 'Calendario',
     'name_fr_FR': 'Calendrier',
     'name_ru_RU': 'Календарь',
-    'version': '2.4.4',
+    'version': '2.4.5',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
